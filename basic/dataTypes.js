edf41c522543e10a97832614bf23cece7eca6cab
Uday Kumar <udaykumar848204@gmail.com>
2024-05-16 02:37:21 +0530

    DataTypes

---
+++ basic/dataTypes.js
@@ -0,0 +1,23 @@
+/*
+"use strict" : treat all js code as newer version
+number
+boolean
+null
+bigint
+string
+undefined
+symbol  : for uniqueNess
+object
+
+typeof null : object
+typeof undefoned : undefined
+*/
+
+"use strict";
+// alert(3 + 3);  // we are using node js not browser
+console.log("Geeky");
+console.log(3 + 3); // code should be readable
+
+let name = "Uday";
+let age = 18;
+let isloggedIn;
