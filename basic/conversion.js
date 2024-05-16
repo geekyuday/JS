addfe9424387820d35c28b620b0ec45522731b95
Uday Kumar <udaykumar848204@gmail.com>
2024-05-16 14:34:14 +0530

    conversion

---
+++ basic/conversion.js
@@ -0,0 +1,25 @@
+let score = "33abc";
+console.log(typeof score)
+
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+/*
+33 : number
+33abc : NaN
+true : 1 , fslse : 0;
+
+*/
+
+let isloggedIn = 1;
+let booelanIslOggedIn = Boolean(isloggedIn)
+console.log(booelanIslOggedIn);
+
+
+let someUmber = 27;
+let stringNUmber = String(someUmber)
+console.log(stringNUmber);
+console.log(typeof stringNUmber);
