ed6c1d0ae394f0893a4b22c27b72fc9988cc92c8
Uday Kumar <udaykumar848204@gmail.com>
2024-05-31 17:22:16 +0530

    higher order loop

---
+++ Iterration/Higher_order_loop.js
@@ -0,0 +1,27 @@
+const arr = [1,2,3,4,5,6,7,8,9]
+
+// for(const num of arr){
+//     console.log(num);
+// }
+
+// const greeting = 'Hello Duiya';
+// for(const greet of greeting){
+//     console.log(greet);
+// }
+
+// Map
+const map = new Map()
+map.set('In',"India")
+map.set('USA',"United State of America")
+map.set('Fr',"France")
+map.set('Chi',"China")
+map.set('In',"India")
+
+//console.log(map);
+
+for(const [key,value] of map){
+    console.log(key," :- ",value)
+}
+
+
+// object is not Iterable using :- for of operator
